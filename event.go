081f97e1c859f525dbeda5d03fa8a36ada40f36d
mohamadrostami <mrostami@harlemnext.com>
2024-04-05 14:04:26 +0200

    test

--- event.go
+++ event.go
@@ -9,12 +9,3 @@ const (
 	EventMissedLookup
 	EventFullRingLookup
 )
-
-func (ch *ConsistentHash) trigger(e Event, data ...any) {
-	if ch.listeners == nil {
-		return
-	}
-	for _, listener := range ch.listeners[e] {
-		listener(e, data...)
-	}
-}
