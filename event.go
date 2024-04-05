9b60fc244ec483f4aace194ba0d2a081e29ef733
mohamadrostami <mrostami@harlemnext.com>
2024-04-05 16:03:59 +0200

    Add Documentation for Exported Types and Variables

--- event.go
+++ event.go
@@ -1,15 +1,21 @@
 package distorage
 
+// Event represents the type of event that can be triggered within distorage.
 type Event uint8
 
+// EventListener defines the function signature for listeners that can react to Events.
 type EventListener func(e Event, data ...any)
 
+// EventReBalance is triggered when the distribution of keys needs to be rebalanced.
+// EventMissedLookup is triggered when a key lookup fails.
+// EventFullRingLookup is triggered when a full ring scan is performed.
 const (
 	EventReBalance Event = 1 << iota
 	EventMissedLookup
 	EventFullRingLookup
 )
 
+// trigger notifies all registered listeners of the given event.
 func (ch *ConsistentHash) trigger(e Event, data ...any) {
 	if ch.listeners == nil {
 		return
